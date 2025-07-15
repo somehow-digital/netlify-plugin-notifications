4e1a8b2cf7f6a79af35d4c116db8e641d447130b
Thomas Deinhamer <184284+thasmo@users.noreply.github.com>
2025-07-15 12:00:14 +0200

    feat: configure event name dynamically (#3)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-export const onSuccess = async function ({ inputs }) {
+const events = {
+	build: 'onBuild',
+	dev: 'onDev',
+	end: 'onEnd',
+	error: 'onError',
+	post_build: 'onPostBuild',
+	pre_build: 'onPreBuild',
+	pre_dev: 'onPreDev',
+	success: 'onSuccess',
+};
+const handler = async ({ inputs }) => {
 	await fetch(inputs.url, {
 		method: 'POST',
 	});
 };
+
+export default function plugin(inputs) {
+	const event = events[inputs.event];
+
+	return {
+		[event]: handler,
+	};
+}
