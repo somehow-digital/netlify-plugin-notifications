2afb02c8663acf7a1b1492c7405ee6b13fe9bbeb
Thomas Deinhamer <code@thasmo.dev>
2025-07-21 09:28:44 +0200

    Adapt project for new eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,3 @@
-import config from '@somehow-digital/eslint-config/basic';
-import globals from 'globals';
+import config from '@somehow-digital/eslint-config';
 
-export default [
-	...config,
-	{
-		ignores: ['**/.*/'],
-		languageOptions: {
-			globals: {
-				...globals.nodeBuiltin,
-			},
-		},
-	},
-];
+export default config();
